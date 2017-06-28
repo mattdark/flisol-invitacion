23370496b9f0d414be0d97df9eaf3da128be471e
Mario Garcia <iscmariog@gmail.com>
2017-06-28 09:53:11 -0500

    Comments added

--- data.py
+++ data.py
@@ -5,17 +5,18 @@ days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Doming
 
 your_date = datetime.date.today()
 month = your_date.month
-year = your_date.year
-month_name = months[month - 1]
-years = (year - 2005) + 1
+year = your_date.year # Today's year
+month_name = months[month - 1] # Today's month
+years = (year - 2005) + 1 # Years of FLISoL
 
 location = "Instituto Universitario de México"
 schedule = "9:00 a 18 horas"
-m = 5
-d = 6
+# Date of the event
+m = 5 # Month
+d = 6 # Day
 dt = datetime.date(year, m, d)
-m = months[m - 1]
-week_day = dt.weekday()
-week_day = days[week_day]
+m = months[m - 1] # Month name
+week_day = dt.weekday() # Day of
+week_day = days[week_day] # the week
 dt = str(week_day) + " " + str(d) + " de " + str(m)
 dt2 = str(d) + " de " + str(m)
